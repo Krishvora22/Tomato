9b776919aac84beeb05de7d4829a47b9c2738d84
Krish Vora <krishvora22@gmail.com>
2025-06-22 19:51:01 +0530

    router bug fix2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,9 +3,9 @@ import Navbar from './components/Navbar/Navbar';
 import {Routes , Route} from 'react-router-dom'
 import Home from './pages/Home/Home';
 import Cart from './pages/Cart/Cart';
-import PlaceOrder from './pages/Placeorder/PlaceOrder';
 import Footer from './components/Footer/Footer';
 import LoginPopUp from './components/LoginPopUp/LoginPopUp';
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder';
 
 const App = () => {
     const [showLogin, setShowLogin] = useState(false)
@@ -17,9 +17,7 @@ const App = () => {
       <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path='/' element={<Home/>} />
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='/order' element={<PlaceOrder/>}/>
-        
+        <Route path='/order' element={<PlaceOrder />} />        
       </Routes>
     </div>
      <Footer/>
