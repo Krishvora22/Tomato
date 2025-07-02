0af15c141778568e6b50c7c445d0d64372621fea
Krish Vora <krishvora22@gmail.com>
2025-07-02 20:34:21 +0530

    some bug

--- frontend/service/cart.api.js
+++ frontend/service/cart.api.js
@@ -3,9 +3,14 @@ import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:1600';
 
 export const addToCartAPI = async ({ userId, foodId }) => {
-  const res = await axios.post(`${BASE_URL}/cart/add`, {
+  try{
+    const res = await axios.post(`${BASE_URL}/cart/add`, {
     userId,
     foodId,
   });
+  } catch (error) {
+    console.error("Error adding to cart:", error);
+    throw error; // Re-throw the error to handle it in the calling function
+  }
   return res.data;
 };
--- frontend/service/order.api.js
+++ frontend/service/order.api.js
@@ -3,6 +3,11 @@ import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:1600';
 
 export const placeOrderAPI = async (orderData) => {
-  const res = await axios.post(`${BASE_URL}/order/placeorder`, orderData);
+  try{
+    const res = await axios.post(`${BASE_URL}/order/placeorder`, orderData);
   return res.data;
+  } catch (error) {
+    console.error("Error placing order:", error);
+    throw error; // Re-throw the error to handle it in the calling function
+  }
 };
