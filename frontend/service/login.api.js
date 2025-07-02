53ece3a65bee4b69940052e7047c3bcb1020d090
Krish Vora <krishvora22@gmail.com>
2025-07-02 20:32:21 +0530

    some bug fix

--- frontend/service/login.api.js
+++ frontend/service/login.api.js
@@ -4,14 +4,25 @@ import axios from 'axios';
 const BASE_URL = import.meta.env.VITE_BACKEND_URL ;
 
 export const signupUser = async (formData) => {
+  try{
   const res = await axios.post(`${BASE_URL}/user/auth/sign-up`, formData);
   return res.data;
+  } catch (error) {
+    console.error("Error during signup:", error);
+    throw error; // Re-throw the error to handle it in the calling function
+  }
+
 };
 
 export const loginuser = async ({email, password}) =>{
-    const res = await axios.post(`${BASE_URL}/user/auth/login` , {
+    try{
+      const res = await axios.post(`${BASE_URL}/user/auth/login` , {
         email , 
         password
     });
+    } catch (error) {
+      console.error("Error during login:", error);
+      throw error; // Re-throw the error to handle it in the calling function
+    }
     return res.data;
 }
