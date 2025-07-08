72e688e4ae2a41c84c414d6aca712fca7312aee8
Krish Vora <krishvora22@gmail.com>
2025-07-08 23:59:39 +0530

    update api

--- Backend/src/modules/cart/cart.controller.ts
+++ Backend/src/modules/cart/cart.controller.ts
@@ -7,13 +7,17 @@ import { ApiOperation } from '@nestjs/swagger';
 export class CartController {
   constructor(private readonly cartService: CartService) {}
 
+  // Create a new cart item
+  @ApiOperation({ summary: 'Add food item to cart' })
  @Post('add')
   async addToCart( @Body() dto: CreateCartDto
   ) {
-            const { userId, foodId } = dto;
+    const { userId, foodId } = dto;
     return this.cartService.addToCart(userId , foodId);
   }
 
+  // Update an existing cart item
+  @ApiOperation({ summary: 'Update food item in cart' })
    @Post('remove')
   async removeFromCart( @Body() dto: CreateCartDto
   ) {
@@ -21,7 +25,8 @@ export class CartController {
     return this.cartService.removeFromCart(userId , foodId);
   }
 
-
+  // Get all items in the cart for a specific user
+  @ApiOperation({ summary: 'Get all food items in cart' })
   @Get('get')
 async getcart(@Query('userId') userId: string) {
   return this.cartService.getcart(userId);
